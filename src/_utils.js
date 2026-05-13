22274cec3a7ed2c19dd2efb67f4048eefd6af14a
modernscape <modernscape@gmail.com>
2026-05-13 10:54:24 +0900

    26-05-13_

--- src/_utils.js
+++ src/_utils.js
@@ -3,8 +3,8 @@
 //////////////////////////////////////////////
 
 const lerp = (x, y, p) => {
-  return x + (y - x) * p;
-};
+  return x + (y - x) * p
+}
 
 //////////////////////////////////////////////
 // シャドーイング
@@ -12,31 +12,31 @@ const lerp = (x, y, p) => {
 
 function setObjectsShadow(objs, castShadow, receiveShadow) {
   for (const obj of objs) {
-    obj.castShadow = castShadow;
-    obj.receiveShadow = receiveShadow;
+    obj.castShadow = castShadow
+    obj.receiveShadow = receiveShadow
   }
 }
 
 //////////////////////////////////////////////
 // _particlesPosition
-//////////////////////////////////////////////
+//////////////////////////////////////////////_particlesPosition
 
-const numParticles = 500_000; // パーティクルの数
+const numParticles = 500_000 // パーティクルの数
 function _particlesPosition(bufferGeometry) {
-  const material = new THREE.MeshBasicMaterial();
-  const mesh = new THREE.Mesh(bufferGeometry.toNonIndexed(), material);
-  const sampler = new THREE.MeshSurfaceSampler(mesh).build();
-  const particlesPosition = new Float32Array(numParticles * 3);
+  const material = new THREE.MeshBasicMaterial()
+  const mesh = new THREE.Mesh(bufferGeometry.toNonIndexed(), material)
+  const sampler = new THREE.MeshSurfaceSampler(mesh).build()
+  const particlesPosition = new Float32Array(numParticles * 3)
   for (let i = 0; i < numParticles; i++) {
-    const newPosition = new THREE.Vector3();
-    const normal = new THREE.Vector3();
+    const newPosition = new THREE.Vector3()
+    const normal = new THREE.Vector3()
 
-    sampler.sample(newPosition, normal);
-    particlesPosition.set([newPosition.x, newPosition.y, newPosition.z], i * 3);
+    sampler.sample(newPosition, normal)
+    particlesPosition.set([newPosition.x, newPosition.y, newPosition.z], i * 3)
   }
-  console.log(particlesPosition);
+  console.log(particlesPosition)
 
-  return particlesPosition;
+  return particlesPosition
 }
 
 //////////////////////////////////////////////
@@ -46,17 +46,17 @@ function _particlesPosition(bufferGeometry) {
 // Float32Array
 // 24-09-05_名前変更　_particlesPosition
 function _particlesPositionForNumber(bufferGeometry, positionsNum) {
-  const material = new THREE.MeshBasicMaterial();
-  const mesh = new THREE.Mesh(bufferGeometry.toNonIndexed(), material);
-  const sampler = new THREE.MeshSurfaceSampler(mesh).build();
-  const particlesPosition = new Float32Array(positionsNum * 3);
+  const material = new THREE.MeshBasicMaterial()
+  const mesh = new THREE.Mesh(bufferGeometry.toNonIndexed(), material)
+  const sampler = new THREE.MeshSurfaceSampler(mesh).build()
+  const particlesPosition = new Float32Array(positionsNum * 3)
   for (let i = 0; i < positionsNum; i++) {
-    const newPosition = new THREE.Vector3();
-    const normal = new THREE.Vector3();
-    sampler.sample(newPosition, normal);
-    particlesPosition.set([newPosition.x, newPosition.y, newPosition.z], i * 3);
+    const newPosition = new THREE.Vector3()
+    const normal = new THREE.Vector3()
+    sampler.sample(newPosition, normal)
+    particlesPosition.set([newPosition.x, newPosition.y, newPosition.z], i * 3)
   }
-  return particlesPosition;
+  return particlesPosition
 }
 
 //////////////////////////////////////////////
@@ -70,15 +70,15 @@ function _perpendicularVector(v1) {
   // v1と平行でない任意のベクトル v2 を選択
   // 例えば、z軸方向の単位ベクトル
   // const v2 = new THREE.Vector3(v1.x + 10, v1.y, v1.z);
-  const v2 = new THREE.Vector3(0, 0, 1);
+  const v2 = new THREE.Vector3(0, 0, 1)
 
   // v1 と v2 の外積を計算
-  const perpendicularVector = new THREE.Vector3();
-  perpendicularVector.crossVectors(v1, v2);
+  const perpendicularVector = new THREE.Vector3()
+  perpendicularVector.crossVectors(v1, v2)
 
   // console.log('直交ベクトル');
   // console.log(perpendicularVector);
-  return perpendicularVector;
+  return perpendicularVector
 }
 
 //////////////////////////////////////////////
@@ -86,52 +86,54 @@ function _perpendicularVector(v1) {
 //////////////////////////////////////////////
 
 function generateTexture_Gaussian() {
-  const canvas = generateCanvas_Gaussian();
-  const texture = new THREE.Texture(canvas);
-  texture.needsUpdate = true;
-  return texture;
+  const canvas = generateCanvas_Gaussian()
+  const texture = new THREE.Texture(canvas)
+  texture.needsUpdate = true
+  return texture
 }
 
 function generateCanvas_Gaussian() {
   //canvas要素の生成
-  var canvas = document.createElement('canvas');
+  var canvas = document.createElement("canvas")
   //canvas要素のサイズ
-  canvas.width = 256; //横幅
-  canvas.height = 256; //縦幅
+  canvas.width = 256 //横幅
+  canvas.height = 256 //縦幅
   //コンテキストの取得
-  var context = canvas.getContext('2d');
+  var context = canvas.getContext("2d")
 
   //ガウス分布の平均値と分散
-  var x_ = canvas.width / 2; //平均値（x座標）
-  var y_ = canvas.height / 2; //平均値（y座標）
-  var sigma2 = 5000; //分散
+  var x_ = canvas.width / 2 //平均値（x座標）
+  var y_ = canvas.height / 2 //平均値（y座標）
+  var sigma2 = 5000 //分散
   //ビットマップデータのRGBAデータ格納配列
-  var bitmapData = [];
+  var bitmapData = []
   //RGBAデータ格納配列への値の代入
   for (var j = 0; j < canvas.height; j++) {
     for (var i = 0; i < canvas.width; i++) {
-      var index = (j * canvas.width + i) * 4; //各ピクセルの先頭を与えるインデクス番号
+      var index = (j * canvas.width + i) * 4 //各ピクセルの先頭を与えるインデクス番号
       var x = i,
-        y = j;
+        y = j
       //ガウス分布の値の取得
-      var f = Math.exp(-((x - x_) * (x - x_) + (y - y_) * (y - y_)) / (2 * sigma2));
+      var f = Math.exp(
+        -((x - x_) * (x - x_) + (y - y_) * (y - y_)) / (2 * sigma2),
+      )
       //ビットマップデータのRGBAデータ
-      bitmapData[index + 0] = 255 * f; //R値
-      bitmapData[index + 1] = 255 * f; //R値
-      bitmapData[index + 2] = 255 * f; //R値
-      bitmapData[index + 3] = 255; //A値
+      bitmapData[index + 0] = 255 * f //R値
+      bitmapData[index + 1] = 255 * f //R値
+      bitmapData[index + 2] = 255 * f //R値
+      bitmapData[index + 3] = 255 //A値
     }
   }
   //イメージデータオブジェクトの生成
-  var imageData = context.createImageData(canvas.width, canvas.height);
+  var imageData = context.createImageData(canvas.width, canvas.height)
   for (var i = 0; i < canvas.width * canvas.height * 4; i++) {
-    imageData.data[i] = bitmapData[i]; //配列のコピー
+    imageData.data[i] = bitmapData[i] //配列のコピー
   }
   //return imageData;
 
   //イメージデータオブジェクトからcanvasに描画する
-  context.putImageData(imageData, 0, 0);
-  return canvas;
+  context.putImageData(imageData, 0, 0)
+  return canvas
 }
 
 //////////////////////////////////////////////
@@ -139,31 +141,31 @@ function generateCanvas_Gaussian() {
 //////////////////////////////////////////////
 
 function generateCanvas_length() {
-  const canvas = document.createElement('canvas');
+  const canvas = document.createElement("canvas")
 
-  canvas.width = 32;
-  canvas.height = 32;
-  const context = canvas.getContext('2d');
+  canvas.width = 32
+  canvas.height = 32
+  const context = canvas.getContext("2d")
 
-  const x_ = canvas.width / 2;
-  const y_ = canvas.height / 2;
+  const x_ = canvas.width / 2
+  const y_ = canvas.height / 2
   // const R = canvas.height / 3;
 
   // 法線マップの場合
-  const bitmapData = [];
+  const bitmapData = []
   for (let j = 0; j < canvas.height; j++) {
     for (let i = 0; i < canvas.width; i++) {
-      const index = (j * canvas.width + i) * 4; // RGBA分
-      const x = i;
-      const y = j;
-      const r = Math.sqrt((x - x_) * (x - x_) + (y - y_) * (y - y_)); // √(x*x) = (y*y)
+      const index = (j * canvas.width + i) * 4 // RGBA分
+      const x = i
+      const y = j
+      const r = Math.sqrt((x - x_) * (x - x_) + (y - y_) * (y - y_)) // √(x*x) = (y*y)
 
-      const v = 256 - (r / (canvas.width / 2)) * 256;
+      const v = 256 - (r / (canvas.width / 2)) * 256
 
-      bitmapData[index + 0] = v;
-      bitmapData[index + 1] = v;
-      bitmapData[index + 2] = v;
-      bitmapData[index + 3] = 255;
+      bitmapData[index + 0] = v
+      bitmapData[index + 1] = v
+      bitmapData[index + 2] = v
+      bitmapData[index + 3] = 255
 
       // bitmapData[index + 0] = 127 + (128 * (x - x_)) / R; //R値
       // bitmapData[index + 1] = 127 + (128 * (y - y_)) / R; //G値
@@ -172,14 +174,14 @@ function generateCanvas_length() {
     }
   }
 
-  const imageData = context.createImageData(canvas.width, canvas.height);
+  const imageData = context.createImageData(canvas.width, canvas.height)
 
   for (let i = 0; i < canvas.width * canvas.height * 4; i++) {
-    imageData.data[i] = bitmapData[i];
+    imageData.data[i] = bitmapData[i]
   }
 
-  context.putImageData(imageData, 0, 0); // 0,0 は描画位置
-  return canvas;
+  context.putImageData(imageData, 0, 0) // 0,0 は描画位置
+  return canvas
 }
 
 //////////////////////////////////////////////
@@ -188,10 +190,14 @@ function generateCanvas_length() {
 //　点(x1, y1, z1) を回転した点
 function _rotationPos(x1, y1, z1, azimuth, elevation) {
   // 方位角 azimuth angle（度）、　仰角 elevation angle（度）
-  const theta = azimuth * (Math.PI / 180); // 方位角
-  const alpha = elevation * (Math.PI / 180); // 仰角
-  const x = (x1 * Math.cos(theta) - y1 * Math.sin(theta)) * Math.cos(alpha) + z1 * Math.sin(alpha);
-  const y = x1 * Math.sin(theta) + y1 * Math.cos(theta);
-  const z = -(x1 * Math.cos(theta) - y1 * Math.sin(theta)) * Math.sin(alpha) + z1 * Math.cos(alpha);
-  return new THREE.Vector3(x, y, z);
+  const theta = azimuth * (Math.PI / 180) // 方位角
+  const alpha = elevation * (Math.PI / 180) // 仰角
+  const x =
+    (x1 * Math.cos(theta) - y1 * Math.sin(theta)) * Math.cos(alpha) +
+    z1 * Math.sin(alpha)
+  const y = x1 * Math.sin(theta) + y1 * Math.cos(theta)
+  const z =
+    -(x1 * Math.cos(theta) - y1 * Math.sin(theta)) * Math.sin(alpha) +
+    z1 * Math.cos(alpha)
+  return new THREE.Vector3(x, y, z)
 }
